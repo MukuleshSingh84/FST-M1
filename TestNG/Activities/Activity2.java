e0202bfe4b7b59dbf6bda1c58c80fc60553c7431
MukuleshSingh84 <147052703+MukuleshSingh84@users.noreply.github.com>
2023-11-21 06:21:04 +0530

    Update Activity2.java

--- TestNG/Activities/Activity2.java
+++ TestNG/Activities/Activity2.java
@@ -1,35 +1,60 @@
 package Activity;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.AfterClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.testng.SkipException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 public class Activity2 {
     WebDriver driver;
-    @BeforeClass
-    public void beforeClass()
-    {
 
+    @BeforeTest
+    public void beforeMethod() {
         // Set up the Firefox driver
-        WebDriverManager.firefoxdriver().setup();
+        //WebDriverManager.firefox().setup();
+        //Create a new instance of the Firefox driver
         driver = new FirefoxDriver();
-        //Open browser
-        driver.get("https://alchemy.hguy.co/jobs");
-        driver.manage().window().maximize();
+
+        //Open the browser
+        driver.get("https://v1.training-support.net/selenium/target-practice");
     }
+
     @Test
-    public void titleValidate()
-    {
-        String strTitle=driver.findElement(By.xpath("//h1[text()='Welcome to Alchemy Jobs']")).getText();
-        Assert.assertEquals("Welcome to Alchemy Jobs", strTitle);
+    public void testCase1() {
+        //This test case will pass
+        String title = driver.getTitle();
+        System.out.println("Title is: " + title);
+        Assert.assertEquals(title, "Target Practice");
     }
-    @AfterClass
-    public void afterClass() {
-        //Close browser
-        driver.quit();
+
+    @Test
+    public void testCase2() {
+        //This test case will Fail
+        WebElement blackButton = driver.findElement(By.cssSelector("button.black"));
+        Assert.assertTrue(blackButton.isDisplayed());
+        Assert.assertEquals(blackButton.getText(), "black");
     }
+
+    @Test(enabled = false)
+    public void testCase3() {
+        //This test will be skipped and not counted
+        String subHeading = driver.findElement(By.className("sub")).getText();
+        Assert.assertTrue(subHeading.contains("Practice"));
+    }
+
+    @Test
+    public void testCase4() {
+        //This test will be skipped and will be be shown as skipped
+        throw new SkipException("Skipping test case");
+    }
+
+    @AfterTest
+    public void afterMethod() {
+        //Close the browser
+        driver.close();
+    }
+
 }
